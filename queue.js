44d87bd2772f91af5d8b0e65fdecf1a55e8567ec
macqueen <jessmacqueen@gmail.com>
2013-12-02 12:16:48 -0800

    convert tabs to spaces

--- queue.js
+++ queue.js
@@ -1,10 +1,10 @@
 // linked list queue implementation
 var linkedQueue = function() {
-	var queue = {};
-	var first = null;
-	var last = null;
+  var queue = {};
+  var first = null;
+  var last = null;
 
-	var makeNode = function(value) {
+  var makeNode = function(value) {
     var node = {};
     node.item = value;
     node.next = null;
@@ -16,25 +16,25 @@ var linkedQueue = function() {
   };
 
   queue.enqueue = function(item) {
-  	var oldLast = last;
-  	last = makeNode(item);
-  	if (queue.isEmpty()) {
-  		first = last;
-  	} else {
-  		oldLast.next = last;
-  	}
+    var oldLast = last;
+    last = makeNode(item);
+    if (queue.isEmpty()) {
+      first = last;
+    } else {
+      oldLast.next = last;
+    }
   };
 
   queue.dequeue = function() {
-  	if (!first) {
-  		throw "Unable to dequeue an empty queue."
-  	}
-  	var item = first.item;
-  	first = first.next;
-  	if (queue.isEmpty()) {
-  		last = null;
-  	}
-  	return item;
+    if (!first) {
+      throw "Unable to dequeue an empty queue."
+    }
+    var item = first.item;
+    first = first.next;
+    if (queue.isEmpty()) {
+      last = null;
+    }
+    return item;
   }
 
   return queue;
